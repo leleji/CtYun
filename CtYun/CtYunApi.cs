0e7ef59d6f5034f16bab6f8cb0d6847904a2a627
乐鸡 <3996@163.com>
2025-12-22 21:37:13 +0800

    fix

--- CtYun/CtYunApi.cs
+++ CtYun/CtYunApi.cs
@@ -28,7 +28,7 @@ namespace CtYun
         public LoginInfo LoginInfo { get; set; }
         public CtYunApi(string deviceCode)
         {
-            _deviceCode = _deviceCode;
+            _deviceCode = deviceCode;
             var handler = new HttpClientHandler();
             client = new HttpClient(handler);
             client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36");
